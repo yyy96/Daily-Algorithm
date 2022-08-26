86a75d26863a7da78e673a033b1bebbffb7d0e61
yyy96 <dlduswo1247@gmail.com>
2022-08-26 17:15:48 +0900

    22.08.26

---
+++ Baekjoon/삼성_기출/13458.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+long n;
+long a, b, c; //범위 주의
+vector<long> room;
+long total = 0;
+
+void solution() {
+	total += n;
+	for (long i = 0; i < n; i++) {
+		long student = room[i];
+		if (student <= b) continue;
+		student -= b;
+
+		long sub = student / c;
+		if (student%c != 0) sub++;
+		total += sub;
+	}
+}
+
+int main() {
+	cin >> n;
+	for (long i = 0; i < n; i++) {
+		cin >> a;
+		room.push_back(a);
+	}
+	cin >> b >> c;
+	solution();
+	cout << total;
+	return 0;
+}
---
+++ Baekjoon/삼성_기출/14501.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+struct Work {
+	int T, P;
+};
+int n;
+vector<Work> schedule;
+int result = 0;
+
+void dfs(int day, int sum) {
+	if (day > n) return;
+	result = max(result, sum);
+
+	for (int i = day; i < n; i++) {
+		dfs(i + schedule[i].T, sum + schedule[i].P);
+	}
+}
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int t, p;
+		cin >> t >> p;
+		schedule.push_back({ t,p });
+	}
+	dfs(0, 0);
+	cout << result;
+	return 0;
+}
