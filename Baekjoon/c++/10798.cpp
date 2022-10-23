01bb90d35b62d2a383e6a5cffb8667f5ea8ca382
yyy96 <dlduswo1247@gmail.com>
2022-10-23 21:29:34 +0900

    22.10.23

---
+++ Baekjoon/c++/10798.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+vector<string> str(5);
+int maxLen = 0;
+string result = "";
+
+int main()
+{
+	for (int i = 0; i < 5; i++) {
+		cin >> str[i];
+		if (str[i].length() > maxLen) maxLen = str[i].length();
+	}
+
+	for (int i = 0; i < maxLen; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (str[j].length() <= i) continue;
+			result += str[j][i];
+		}
+	}
+	cout << result;
+	return 0;
+}
