4a5b6f6557128538134d3eb470eeaf2492e4384f
yyy96 <dlduswo1247@gmail.com>
2022-11-05 08:38:25 +0900

    22.11.05

--- Baekjoon/c++/6987.cpp
+++ Baekjoon/c++/6987.cpp
@@ -1,60 +1,75 @@
 #include <iostream>
-#include <string>
-#include <cstring>
-#include <sstream>
-#include <queue>
-#include <stack>
 #include <vector>
-#include <unordered_map>
 #include <algorithm>
 using namespace std;
 
-int team[6][3];
-int result[4];
+struct Team { int a, b; };
+int score[6][3];
+int input[6][3][4];
+int result[4] = { 0, };
 
-int solution() {
-	//조건1. 각 나라의 승+무+패 = 5
+vector<Team> team;
+int path[2];
+
+bool isValid(int test) {
 	for (int i = 0; i < 6; i++) {
-		if (team[i][0] + team[i][1] + team[i][2] != 5)
-			return 0;
+		for (int j = 0; j < 3; j++) {
+			if (input[i][j][test] != score[i][j]) return false;
+		}
+	}
+	return true;
+}
+
+//서로 가질 수 있는 조합
+void scoreDfs(int lev) {
+	if (lev == team.size()) {
+		//만약 현재 score가 input 중에 있다면 해당 result는 1처리
+		for (int t = 0; t < 4; t++) {
+			if (result[t] == 1) continue;
+			if (isValid(t)) result[t] = 1;
+		}
+		return;
 	}
 
-	//조건2. 총 승 = 패의 수는 같아야한다.
-	int win = 0, lose = 0;
-	for (int i = 0; i < 6; i++) {
-		win += team[i][0];
-		lose += team[i][2];
-	}
-	if (win != lose) return 0;
+	//승패
+	score[team[lev].a][0]++; score[team[lev].b][2]++;
+	scoreDfs(lev + 1);
+	score[team[lev].a][0]--; score[team[lev].b][2]--;
 
-	//조건3-1. 무의 총 횟수는 홀수일 수 없다.
-	int same = 0;
-	for (int i = 0; i < 6; i++) {
-		same += team[i][1];
-	}
-	if (same % 2 != 0) return 0;
+	//패승
+	score[team[lev].a][2]++; score[team[lev].b][0]++;
+	scoreDfs(lev + 1);
+	score[team[lev].a][2]--; score[team[lev].b][0]--;
 
-	//조건3-2. 무의 조합이 이루어져야한다.
-	same = 0;
-	for (int i = 0; i < 6; i++) {
-		if (same <=0 ) same += team[i][1];
-		else same -= team[i][1];
+	//무무
+	score[team[lev].a][1]++; score[team[lev].b][1]++;
+	scoreDfs(lev + 1);
+	score[team[lev].a][1]--; score[team[lev].b][1]--;
+}
+
+//서로 경기할 수 있는 나라들 조합
+void gameDfs(int lev, int start) {
+	if (lev == 2) {
+		team.push_back({ path[0], path[1] });
+		return;
 	}
-	if (same != 0) return 0;
-	
-	return 1;
+
+	for (int i = start; i < 6; i++) {
+		path[lev] = i;
+		gameDfs(lev + 1, i + 1);
+		path[lev] = 0;
+	}
 }
 
 int main() {
 	for (int t = 0; t < 4; t++) {
 		for (int i = 0; i < 6; i++) {
 			for (int j = 0; j < 3; j++)
-				cin >> team[i][j];
+				cin >> input[i][j][t];
 		}
-		result[t] = solution();
 	}
+	gameDfs(0, 0);
+	scoreDfs(0);
 	for (int i = 0; i < 4; i++)
 		cout << result[i] << " ";
-
-	return 0;
 }
